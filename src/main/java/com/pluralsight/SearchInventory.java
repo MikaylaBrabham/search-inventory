9938929930edd6078ed30c643d3c7781c8c0da8a
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-04-22 15:17:27 -0500

    add boba shop products to getinventory method

--- src/main/java/com/pluralsight/SearchInventory.java
+++ src/main/java/com/pluralsight/SearchInventory.java
@@ -6,9 +6,19 @@ import java.util.Scanner;
 public class SearchInventory {
 
     public static void main(String[] args) {
+    }
 
-        // insert getinventory method here to create list of products
-        ArrayList<String> getInventory = new ArrayList<>();
+    // insert getinventory method here to create list of products
+    public static void getInventory() {
+
+       // create an empty list of products
+        ArrayList<String> products = new ArrayList<>();
+        // add a minimum of 5 products to the list
+        products.add("Black pearls");
+        products.add("Popping Boba");
+        products.add("Tapioca Starch")
+
+        {
 
 
 
