49589e262f0315491bafcc4ad8e10865494bc00c
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-04-22 15:02:29 -0500

    add getinventory list

--- src/main/java/com/pluralsight/SearchInventory.java
+++ src/main/java/com/pluralsight/SearchInventory.java
@@ -1,4 +1,24 @@
 package com.pluralsight;
+// import arraylist and scanner
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class SearchInventory {
+
+    public static void main(String[] args) {
+
+        // insert getinventory method here to create list of products
+        ArrayList<String> getInventory = new ArrayList<>();
+
+
+
+
+
+        System.out.println("Please make a selection from our current inventory: ");
+
+    }
+
+    }
+
+
 }
