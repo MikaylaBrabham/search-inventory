7d34bf7fde0550160b693c91fd6311e737a2110f
Mikayla Brabham <mikaylabrabham01@gmail.com>
2026-05-25 17:47:38 -0500

    add product class and variables necessary for products

--- src/main/java/com/pluralsight/Products.java
+++ src/main/java/com/pluralsight/Products.java
@@ -3,12 +3,29 @@ package com.pluralsight;
 public class Products {
 
     //add variables
-    private int id:
+    private int id;
     private String name;
     private float price;
 
     //add constructors
 
+    public Products(int id, String name, float price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
     //add getters
 
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
 }
